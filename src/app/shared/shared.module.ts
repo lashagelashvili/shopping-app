ce6aa3a66db67eaab87362cd33c158b9105ad470
Lasha Gelashvili <las.gelashvili@gmail.com>
2023-02-13 23:43:29 +0400

    add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { BackArrowComponent } from './components/back-arrow/back-arrow.component';
+
+@NgModule({
+  declarations: [BackArrowComponent],
+  imports: [],
+  exports: [BackArrowComponent],
+})
+export class SharedModule {}
