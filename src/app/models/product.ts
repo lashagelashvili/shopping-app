7dbf306483d3570b8eed45ece72398256f7d77fe
Lasha Gelashvili <las.gelashvili@gmail.com>
2023-02-16 00:41:51 +0400

    add product model

---
+++ src/app/models/product.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  rating: Rating;
+  title: string;
+  amount: number;
+}
+
+interface Rating {
+  rate: number;
+  count: number;
+}
