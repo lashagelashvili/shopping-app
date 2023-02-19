edfa1cd1c66cecafe56f1d50f7df2232f3f2e387
Lasha Gelashvili <las.gelashvili@gmail.com>
2023-02-19 23:28:38 +0400

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { HomepageComponent } from './layout/homepage/homepage.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'shopping-list', pathMatch: 'full' },
   { path: 'shopping-list', component: HomepageComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: '**', redirectTo: 'shopping-list' },
 ];
 
 @NgModule({
