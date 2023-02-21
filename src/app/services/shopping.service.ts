7557ad206ee9c37ca4b1bd3d5296fe6329322216
Lasha Gelashvili <las.gelashvili@gmail.com>
2023-02-22 00:25:55 +0400

    update service

--- src/app/services/shopping.service.ts
+++ src/app/services/shopping.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -8,9 +9,53 @@ import { Observable } from 'rxjs';
 export class ShoppingService {
   private _http = inject(HttpClient);
 
+  selectedProducts: Product[] = [];
+
+  $selectedProducts = new BehaviorSubject<Product[] | null>(null);
+  selectedProducts$ = this.$selectedProducts.asObservable();
+
   constructor() {}
 
-  getItems(): Observable<any> {
-    return this._http.get('https://fakestoreapi.com/products');
+  getItems(): Observable<Product[]> {
+    return this._http.get<Product[]>('https://fakestoreapi.com/products');
+  }
+
+  clearSelectedProducts() {
+    this.selectedProducts = [];
+    this.$selectedProducts.next(null);
+  }
+
+  addProduct(product: Product) {
+    if (this.selectedProducts.length === 0) {
+      product.amount = 1;
+      this.selectedProducts.push(product);
+      this.$selectedProducts.next(this.selectedProducts);
+      return;
+    }
+
+    for (let i = 0; i < this.selectedProducts.length; i++) {
+      if (product.id === this.selectedProducts[i].id) {
+        this.selectedProducts[i].amount++;
+        return;
+      }
+    }
+    product.amount = 1;
+    this.selectedProducts.push(product);
+    this.$selectedProducts.next(this.selectedProducts);
+  }
+
+  calculateTotal() {
+    return this.selectedProducts.reduce((acc, current) => {
+      let total = current.price * current.amount;
+      return acc + total;
+    }, 0);
+
+    console.log(this.selectedProducts);
+  }
+
+  removeDuplicates() {
+    return this.selectedProducts.filter(
+      (item, index) => this.selectedProducts.indexOf(item) === index
+    );
   }
 }
