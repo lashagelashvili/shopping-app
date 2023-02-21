8ef74302f8e15cd95248d6b702395645f5f90503
Lasha Gelashvili <las.gelashvili@gmail.com>
2023-02-22 00:26:03 +0400

    add shopping list component

---
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { shareReplay, tap } from 'rxjs';
+import { Product } from 'src/app/models/product';
+import { ShoppingService } from 'src/app/services/shopping.service';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.scss'],
+})
+export class ShoppingListComponent implements OnInit {
+  private readonly _shoppingService = inject(ShoppingService);
+  private readonly _router = inject(Router);
+
+  products$ = this._shoppingService.getItems().pipe(tap(console.log));
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  addItem(product: Product) {
+    this._shoppingService.addProduct(product);
+  }
+
+  goToCart() {
+    this._router.navigateByUrl('/shopping-cart');
+  }
+}
