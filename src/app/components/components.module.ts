e2ec3e9ce20091ef924dfdea4b0ae1e5e259bab3
lashagelashvili <las.gelashvili@gmail.com>
2023-03-09 01:40:55 +0400

    add login component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,12 +5,14 @@ import { ShoppingItemComponent } from './shopping-item/shopping-item.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     ShoppingListComponent,
     ShoppingItemComponent,
     ShoppingCartComponent,
+    LoginComponent,
   ],
   imports: [CommonModule, SharedModule, FormsModule],
   exports: [ShoppingListComponent, ShoppingItemComponent],
