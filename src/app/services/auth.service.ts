46d25da666f812b06834ffb3e6b4e93b3888ea33
lashagelashvili <las.gelashvili@gmail.com>
2023-03-09 00:24:04 +0400

    add create user method using angular firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private readonly auth: AngularFireAuth) {}
 
-  constructor() { }
+  createUser(email: string, password: string) {
+    this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((res) => console.log(res));
+  }
 }
