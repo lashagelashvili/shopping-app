d2e081010db50b42af3c8bd6445c1f583b6d047d
lashagelashvili <las.gelashvili@gmail.com>
2023-03-09 01:41:18 +0400

    add route for login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { HomepageComponent } from './layout/homepage/homepage.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'shopping-list', component: HomepageComponent },
   { path: 'shopping-cart', component: ShoppingCartComponent },
   { path: '**', redirectTo: 'shopping-list' },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
