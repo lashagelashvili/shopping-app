2539a6a82e90f17460ad96392dc83892348d2755
lashagelashvili <las.gelashvili@gmail.com>
2023-03-11 03:26:02 +0400

    add route for signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { SignupComponent } from './components/signup/signup.component';
 import { HomepageComponent } from './layout/homepage/homepage.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'shopping-list', component: HomepageComponent },
   { path: 'shopping-cart', component: ShoppingCartComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   { path: '**', redirectTo: 'shopping-list' },
 ];
 
